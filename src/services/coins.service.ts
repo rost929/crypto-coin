99dd61ff0233d3228ba7d7ce89e9b413255ac985
Juan Pena <juan.pena@wmcontractor.com>
2022-09-21 19:33:16 -0500

    Appliying config envs

--- src/services/coins.service.ts
+++ src/services/coins.service.ts
@@ -1,30 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import { Coin } from "src/entities/coin.entity";
 import * as dotenv from 'dotenv';
-//import { ConfigService } from "@nestjs/config";
+import { ConfigService } from "@nestjs/config";
 
-dotenv.config();
+//dotenv.config();
 
 const axios = require('axios');
 
-const BASE_URL = process.env.BASE_API_URL;
+//const BASE_URL = process.env.BASE_API_URL;
 
 @Injectable()
 export class CoinsService {
 
     constructor(
-        //private configService : ConfigService,
+        private configService : ConfigService,
     ) {}
 
     async getCoinList(): Promise<Coin[]> {
         try {
-            //const BASE_URL = this.configService.get('BASE_API_URL');
-            //console.log(BASE_URL);
-            
+            const BASE_URL = this.configService.get('BASE_API_URL'); 
+            const MESSAGE = this.configService.get('MESSAGE');           
             const coinList = await axios.get(`${BASE_URL}/coins/list`);
-            //const bitcoin = coinList.data.filter(currency => currency.name === 'Bitcoin');
-            //console.log(bitcoin);
-            console.log("Var env", process.env.MESSAGE);
+            console.log("Var env", MESSAGE);
             return coinList.data;
         } catch (error) {
             throw new Error("Error getting coin lists");
@@ -33,6 +30,7 @@ export class CoinsService {
 
     async getCoinById(id: string) {
         try {
+            const BASE_URL = this.configService.get('BASE_API_URL');
             const coinList = await axios.get(`${BASE_URL}/coins/${id}`);
             return coinList.data;
         } catch (error) {
@@ -42,6 +40,7 @@ export class CoinsService {
 
     async getCoinsMarket(currency: string) {
         try {
+            const BASE_URL = this.configService.get('BASE_API_URL');
             const marketList = await axios.get(`${BASE_URL}/coins/markets?vs_currency=${currency}`);
             return marketList.data;
         } catch (error) {
