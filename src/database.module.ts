a212f9f1531925ee6cce363bf4b307e844365d69
rost929 <rost929@gmail.com>
2022-11-15 20:35:35 -0500

    New env configuration

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,23 +1,27 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
+import config from './config';
 
 @Global()
 @Module({
   providers: [
     {
       provide: 'MONGO',
-      useFactory: async () => {
+      useFactory: async (configService: ConfigType<typeof config>) => {
         try {
+          const { dbName , user, password, host, port, connection} = configService.mongo;
           const uri =
-            'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT';
+            `${connection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
           const client = new MongoClient(uri);
           await client.connect();
-          const database = client.db('crypto-coin');
+          const database = client.db(dbName);
           return database;
         } catch (error) {
           throw new Error('Error when trying to connect to mongo DB');
         }
       },
+      inject: [config.KEY]
     },
   ],
   exports: ['MONGO'],
